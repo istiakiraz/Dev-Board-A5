f7bcfee1a83d555f6e010d97ff576c52efcfcda9
istiakiraz <istiakiraz@gmail.com>
2025-03-01 02:13:06 +0600

    added first page

---
+++ js/time.js
@@ -0,0 +1,29 @@
+function displayTodayDate() {
+    const today = new Date();
+
+    const weekday = today.toLocaleString('en-BD', { weekday: 'short' });
+    const month = today.toLocaleString('en-BD', { month: 'short' });
+    const day = today.getDate();
+    const year = today.getFullYear();
+
+
+    document.getElementById("weekday").innerText = weekday;
+    document.getElementById("month-day-year").innerHTML = `${month} ${day} ${year}`;
+}
+
+
+
+
+window.onload = displayTodayDate;
+
+
+        // function displayTodayDate(){
+        //     const todayDate = new Date();
+        //     const optionsDate = { 
+        //         weekday: 'short', month: 'short', day: 'numeric', year: 'numeric' 
+        //     };
+
+        //     const formattedDated = todayDate.toLocaleDateString(undefined, optionsDate);
+
+        //     document.getElementById("date").innerText = `${formattedDated}`;
+        // }
